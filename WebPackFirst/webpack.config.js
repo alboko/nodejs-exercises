2beb9afa3d30348e5d55355184ceeed32a61dcd5
alboko <alboko@alekotra.com>
2016-07-17 21:21:58 -0400

    complete first build for WebPackFirst

---
+++ WebPackFirst/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+
+var config = {
+    entry: './src',               // entry point
+    output: {                     // output folder
+        path: './dist',           // folder path
+        filename: 'my-app.js'     // file name
+    }
+}
+
+module.exports = config;
